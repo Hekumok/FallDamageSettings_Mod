ad51d6fe5997785e547e6311bb86b9b06550a0ac
Artemov Nikita <neki712@yandex.ru>
2019-10-01 02:17:23 +0300

    add hook to fix minecraft bug which deal incorrect damage at fall

--- src/main/java/com/hekumok/fallsettings/FallSettingsModHooks.java
+++ src/main/java/com/hekumok/fallsettings/FallSettingsModHooks.java
@@ -1,4 +1,15 @@
 package com.hekumok.fallsettings;
 
+import com.hekumok.hooklib.asm.*;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+
 public class FallSettingsModHooks {
+    @Hook(at = @At(point = InjectionPoint.HEAD))
+    public static void updateFallState(Entity entity, double y, boolean onGroundIn, IBlockState state, BlockPos pos) {
+        if(onGroundIn && entity.fallDistance > 0.0F && y < 0.0D) {
+            entity.fallDistance = (float)((double)entity.fallDistance - y);
+        }
+    }
 }
