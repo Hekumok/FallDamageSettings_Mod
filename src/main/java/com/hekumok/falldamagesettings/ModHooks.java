28e55f1b8eca1fee4b1e1d8b390b68894e474b67
Artemov Nikita <neki712@yandex.ru>
2019-10-05 21:43:02 +0300

    add comments

--- src/main/java/com/hekumok/falldamagesettings/ModHooks.java
+++ src/main/java/com/hekumok/falldamagesettings/ModHooks.java
@@ -11,9 +11,13 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 
 public class ModHooks {
+    /*
+     * Fix old Minecraft bug and calculate distance of fall into water/lava
+     */
     @Hook(at = @At(point = InjectionPoint.HEAD), setLocalVar = 3)
     public static boolean updateFallState(Entity entity, double _y, boolean onGroundIn, IBlockState state, BlockPos pos) {
         if((entity.isInWater() || entity.isInLava()) && ModConfig.getSettingsForEntity(entity).active && entity.fallDistance > 0.0F) {
+            // this hack with onGroundIn is necessary for falling into water/lava to work just like falling to the ground
             onGroundIn = true;
 
             Material material = entity.isInWater() ? Material.WATER : Material.LAVA;
@@ -31,15 +35,18 @@ public class ModHooks {
                         ": Smth went wrong while calculating the entry point into water/lava (when falling)");
             }
 
+            // find entry point into water/lava
             do {
                 bp = bp.up();
             } while(entity.world.getBlockState(bp).getMaterial() == material);
 
             int entryPointY = bp.getY();
 
+            // we can fall under lava/water but damage should be calculated as if we stay on the top of water/lava
             entity.fallDistance -= entryPointY - entity.posY;
         }
 
+        // fix Minecraft bug
         if(onGroundIn && _y < 0.0D) {
             entity.fallDistance = (float)((double)entity.fallDistance - _y);
         }
@@ -47,6 +54,7 @@ public class ModHooks {
         return onGroundIn;
     }
 
+    // fix fall distance when jump into lava
     @Hook(at = @At(point = InjectionPoint.RETURN))
     public static void onEntityUpdate(Entity entity) {
         if (entity.isInLava() && ModConfig.getSettingsForEntity(entity).active) {
@@ -54,6 +62,7 @@ public class ModHooks {
         }
     }
 
+    // fix fall distance when jump into water
     @Hook(at = @At(point = InjectionPoint.HEAD))
     public static void updateFallState(EntityLivingBase entity, double y, boolean onGroundIn, IBlockState state, BlockPos pos) {
         float fallDistance = entity.fallDistance;
@@ -63,6 +72,7 @@ public class ModHooks {
         }
     }
 
+    // calculate new fall damage
     @Hook(at = @At(point = InjectionPoint.VAR_ASSIGNMENT, targetVar = 6, ordinal = 0), setLocalVar = 6)
     public static int fall(EntityLivingBase entity, float distance, float damageMultiplier, @Hook.LocalVariable(6) int originalVar) {
         ModConfig.FallDamageSettings config = ModConfig.getSettingsForEntity(entity);
